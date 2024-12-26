de8a00fc098ce62fb95f761f88f0af4b650f0240
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2024-12-26 20:36:36 +0530

    Update app.module.ts

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    FormComponent, // Register the Form component
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [FormComponent] // Set FormComponent as the root component
+   declarations: [
+       
+   ],
+   imports: [
+       
+       HttpClientModule,
+       FormsModule,
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
  
