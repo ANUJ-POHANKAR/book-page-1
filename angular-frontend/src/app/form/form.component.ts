f72d5ebdbbf4f427492465994155ce538ec5794b
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2024-12-26 20:33:51 +0530

    Update form.component.ts

--- angular-frontend/src/app/form/form.component.ts
+++ angular-frontend/src/app/form/form.component.ts
@@ -1,29 +1,25 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { OrderService } from '../services/order.service';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  formData = {
-    cusname: '',
-    telephone: '',
-    mail: '',
-    book: '',
-    quantity: 0
-  };
+    constructor(private orderService: OrderService) {}
 
-  constructor(private http: HttpClient) {}
-
-  onSubmit() {
-    this.http.post('http://localhost:8080/api/submit', this.formData)
-      .subscribe(response => {
-        console.log('Form submitted successfully:', response);
-      }, error => {
-        console.error('Error submitting form:', error);
-      });
-  }
+    onSubmit(form: any): void {
+        const order = form.value;
+        this.orderService.saveOrder(order).subscribe(
+            response => {
+                console.log('Order submitted successfully', response);
+            },
+            error => {
+                console.error('Error submitting order', error);
+            }
+        );
+    }
 }
+
 
